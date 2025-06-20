f94026fa63d07aefa1b1b4e9b0cdf53800b96849
HEang6868 <eanghilton@gmail.com>
2025-06-19 20:35:07 -0700

    Fourth version of this tool, but the first one that I'm backing up with github. Still learning how to use it and integrate it into my workflow.

--- Utils.py
+++ Utils.py
@@ -1,6 +1,7 @@
 import maya.cmds as mc
 
-
+# import imp
+# imp.reload(mc)
 
 
 # def inputObj(self, field):
@@ -18,3 +19,98 @@ import maya.cmds as mc
 #             textField = textField[:-1]
 #             lastChar = textField[-1]
 #         print(textField)
+
+
+def poseFreeze(obj, posFrame, freezeLen): #, animLayer=False 
+    """
+    Saves a given object's position and rotation, and freezes it in world space for a given frame range to match its tranforms on a given frame.
+    """
+    mc.currentTime(posFrame)
+    pos = mc.xform(obj, q=True, ws=True, t=True)
+    rot = mc.xform(obj, q=True, ws=True, ro=True)
+    print(f"{obj=}\n{posFrame=}, {freezeLen=}\n{pos=} {rot=}")
+    for frm in range(posFrame, (posFrame+freezeLen)):
+        mc.currentTime(frm)
+        mc.xform(obj, ws=True,t=pos, ro=rot)
+        mc.setKeyframe(obj, 
+                        attribute=["translateX", "translateY", "translateZ", "rotateX", "rotateY", "rotateZ"]
+                        )
+        print(f"keyFrame {frm} Set")
+        
+
+
+#poseFreeze("R_Hand", int(mc.currentTime(q=True)), 3)
+
+def funcLoop(start, end, cycleLen, obj, stepA, stepB, animLayer):
+    """
+    Runs poseFreeze() and animLyrZero() functions for a given object for every loop.
+    """
+    for frame in range(start, end, cycleLen):
+        focusAnimLyr(animLayer)
+        poseFreeze(obj=obj, posFrame=frame+stepA, freezeLen=stepB-stepA)
+        # if animLayer:
+        #     resetFrame = int( (cycleLen + cycleLen - stepB) / 2 )
+        #     animLyrZero(obj=obj, frame=resetFrame, layer=animLayer)
+
+
+# control = "R_Hand"
+# startFrame = 1
+# endFrame = 110
+# freezeLen = 3
+# cycle = 15
+# stepA = 3
+# stepB = 6
+# animLayer = "Test"
+
+# funcLoop(startFrame, endFrame, cycle, control, stepA, stepB, animLayer)
+
+# control = "L_Hand"
+# startFrame = 1
+# endframe = 110
+# freezeLen = 3
+# cycle = 15
+# stepA = 3
+# stepB = 6
+
+# funcLoop(startFrame, endframe, cycle, control, stepA, stepB)
+
+
+def addToAnimLYR(obj=False, layer=False):
+    """
+    Checks for an animLayer and creates it if it doesn't exist. Adds a given object to the layer.
+    """
+    #Clears selection and selects the given objects.
+    mc.select(clear=True)
+    print(f"Adding {obj} to animLayer: {layer}.")
+    mc.select(obj)
+    #If the animLayer already exists add the object to the layer.
+    if mc.animLayer(layer, q=True, exists=True):
+        mc.animLayer(layer, e=True, addSelectedObjects=True)
+    #Otherwise, make the layer and add the object to it.
+    else:
+        mc.animLayer(layer, addSelectedObjects=True)
+
+# addToAnimLYR("pCube1","Front_LYR")
+# addToAnimLYR("pCube2","Front_LYR")
+
+def focusAnimLyr(layer):
+    """
+    Deselect all animLayers then select the created animLayer.
+    """
+    animLayers = mc.ls(type="animLayer")
+    for animLyr in animLayers:
+        mc.animLayer(animLyr, e=True, selected=False)
+    mc.animLayer(layer, e=True, selected=True)
+
+
+
+def animLyrZero(obj, frame, layer):
+    """
+    Zeroes out a given object's animation on a given animLayer.
+    """
+    print("zeroing out")
+    focusAnimLyr(layer)
+    mc.setKeyframe(obj, attribute=["translateX", "translateY", "translateZ", "rotateX", "rotateY", "rotateZ"], time=frame)
+    mc.keyframe(obj, e=True, attribute=["translateX", "translateY", "translateZ", "rotateX", "rotateY", "rotateZ"], absolute=True, valueChange=0, time=(frame,))
+
+#animLyrZero("R_Hand", 11, "test")
--- cycleCleaner.py
+++ cycleCleaner.py
@@ -1,5 +1,14 @@
 import maya.cmds as mc
+import cycleCleaner.utils as cUt
+import imp
+imp.reload(cUt)
+from cycleCleaner.utils import funcLoop, addToAnimLYR, focusAnimLyr
 
+
+
+###############################
+###   WINDOW CONSTRUCTION   ###
+###############################
 
 class CycleCleanup():
     """
@@ -7,8 +16,8 @@ class CycleCleanup():
     """
     def __init__(self):
         self.winName = "WalkCycleCleanup"
-        self.winWidth = 430
-        self.winHeight = 290
+        self.winWidth = 440
+        self.winHeight = 360
 
         if mc.window(self.winName, exists=True):
             mc.deleteUI(self.winName)
@@ -35,12 +44,12 @@ class CycleCleanup():
                                            )
         #Create a layout to hold the frame range inputs.
         fRangeLayout = mc.rowLayout(numberOfColumns=3)
-        self.fRangeInputA = mc.textFieldGrp(parent=fRangeLayout, 
+        self.fRangeStartInput = mc.textFieldGrp(parent=fRangeLayout, 
                                        label="Frame Range: ",
                                         columnAlign=(1, "left"),
                                         columnWidth2=(110, 50),
                                         )
-        self.fRangeInputB = mc.textFieldButtonGrp(parent=fRangeLayout, 
+        self.fRangeEndInput = mc.textFieldButtonGrp(parent=fRangeLayout, 
                                        label="  -  ",
                                         columnAlign=(1, "left"),
                                         columnWidth3=(20, 50, 20),
@@ -48,12 +57,50 @@ class CycleCleanup():
                                         buttonCommand=self.getFrameRange
                                         )
         #Create the starting foot radioButtonGrp.
-        self.startFootRBtns = mc.radioButtonGrp(label="Starting Foot",
+        self.startFootRBtns = mc.radioButtonGrp(label="Starting Foot: ",
+                                                parent=mainFormLayout,
                                                 numberOfRadioButtons=2,
                                                 labelArray2=["Right", "Left"],
-                                                columnWidth2=(50, 50)
+                                                columnAlign3=("left", "center", "center"),
+                                                columnWidth3=(110, 80, 50)
                                                 )
-        
+        #Create the layout to hold the cycle length input.
+        stepLenLayout = mc.rowLayout(parent=mainFormLayout, numberOfColumns=2)
+        self.stepLenInput = mc.textFieldGrp(label="Cycle Length: ",
+                                             parent=stepLenLayout,
+                                            columnAlign=(1, "left"),
+                                            columnWidth2=(110, 50),
+                                            )
+        mc.text("The frame range of one step.", parent=stepLenLayout)
+
+        #Create a layout to hold the frame range inputs.
+        stepFramesLayout = mc.rowLayout(parent=mainFormLayout, numberOfColumns=4, columnAlign=(3, "left"))
+        self.stepStartInput = mc.textFieldGrp(parent=stepFramesLayout, 
+                                       label="Step Frames: ",
+                                        columnAlign=(1, "left"),
+                                        columnWidth2=(110, 50),
+                                        )
+        self.stepEndInput = mc.textFieldGrp(parent=stepFramesLayout, 
+                                       label="  -  ",
+                                        columnAlign=(1, "left"),
+                                        columnWidth2=(20, 50),
+                                        )
+        mc.text("The frames in the cycle when\n the foot is on the ground.", parent=stepFramesLayout)
+
+        #Create a layout to hold the animLayer options.
+        animLYRLayout = mc.rowLayout(parent=mainFormLayout, numberOfColumns=4)
+        mc.text("Anim Layer: ", parent=animLYRLayout, width=110, align="left")
+        self.animLYRChkBox = mc.checkBox(label="", 
+                                         parent=animLYRLayout,
+                                         width=20,
+                                         changeCommand=self.UICheck
+                                         )
+        self.animLYRNameInput = mc.textFieldGrp(placeholderText="Layer Name",
+                                                parent=animLYRLayout,
+                                                width=130,
+                                                enable=False,
+                                                )
+
         #Add the controls to the form layout.
         mc.formLayout(mainFormLayout, e=True, 
                     attachForm=( [self.rFootInput, "left", 5],
@@ -64,23 +111,31 @@ class CycleCleanup():
                                 [fRangeLayout, "left", 5],
                                 [fRangeLayout, "right", 5],
                                 [self.startFootRBtns, "left", 5],
-                                [self.startFootRBtns, "right", 5] 
+                                [self.startFootRBtns, "right", 5],
+                                [stepLenLayout, "left", 5],
+                                [stepLenLayout, "right", 5],
+                                [stepFramesLayout, "left", 5],
+                                [stepFramesLayout, "right", 5],
+                                [animLYRLayout, "left", 5],
+                                [animLYRLayout, "right", 5]   
                                  ),
                     attachControl=( [self.lFootInput, "top", 5, self.rFootInput],
                                    [fRangeLayout, "top", 8, self.lFootInput],
-                                   [self.startFootRBtns, "top", 8, fRangeLayout]
+                                   [self.startFootRBtns, "top", 8, fRangeLayout],                                   
+                                   [stepLenLayout, "top", 8, self.startFootRBtns],                                   
+                                   [stepFramesLayout, "top", 8, stepLenLayout],                                   
+                                   [animLYRLayout, "top", 8, stepFramesLayout],                                   
                                      )
                     )
-
-        
-
-
         #Button that runs the script.
-        #mc.button(label="Cleanup Walk Cycle", parent=mainLayout, command=self.cycleCleanup)
+        mc.button(label="Clean Up walk Cycle!!", parent=mainLayout, command=self.cycleClean)
 
         mc.showWindow()
 
 
+########################
+###   UI FUNCTIONS   ###
+########################
 
     def inputObj(self, field):
         """
@@ -101,10 +156,77 @@ class CycleCleanup():
         else:
             start = mc.playbackOptions(q=True, minTime=True)
             end = mc.playbackOptions(q=True, maxTime=True)
-        mc.textFieldGrp(self.fRangeInputA, e=True, text=start)
-        mc.textFieldGrp(self.fRangeInputB, e=True, text=end)
-        
+        mc.textFieldGrp(self.fRangeStartInput, e=True, text=start)
+        mc.textFieldGrp(self.fRangeEndInput, e=True, text=end)
+    
 
+    def UICheck(self, *args):
+        """
+        Checks the animLayer checkbox and toggles the matching textField.
+        """
+        getChkboxChk = mc.checkBox(self.animLYRChkBox, q=True, value=True)
+        mc.textFieldGrp(self.animLYRNameInput, e=True, enable=getChkboxChk)
+    
+
+##########################
+###   TOOL FUNCTIONS   ###
+##########################
+
+    def getData(self, *args):
+        """
+        Checks that the window is filled out properly and gets the data in it if it is.
+        """
+        try:
+            self.getRFootCtrl = mc.textFieldButtonGrp(self.rFootInput, q=True, text=True)
+            self.getLFootCtrl = mc.textFieldButtonGrp(self.lFootInput, q=True, text=True)
+            self.getFRangeStart = int(float(mc.textFieldGrp(self.fRangeStartInput, q=True, text=True)))
+            self.getFRangeEnd = int(float(mc.textFieldGrp(self.fRangeEndInput, q=True, text=True)))
+            self.getStartFoot = int(float(mc.radioButtonGrp(self.startFootRBtns, q=True, sl=True)))
+            self.getCycle = int(mc.textFieldGrp(self.stepLenInput, q=True, text=True))
+            self.getStepA = int(mc.textFieldGrp(self.stepStartInput, q=True, text=True))
+            self.getStepB = int(mc.textFieldGrp(self.stepEndInput, q=True, text=True))
+            self.animLYRName = None
+            if mc.textFieldGrp(self.animLYRNameInput, q=True, enable=True):
+                self.animLYRName = mc.textFieldGrp(self.animLYRNameInput, q=True, text=True)
+        except ValueError:
+            return print("ERROR: The window is missing information. Double check that you've filled out every section.")
+        print("Cleaning walk cycle!")
+        print(self.getRFootCtrl, self.getLFootCtrl, self.getFRangeStart, self.getFRangeEnd, self.getStartFoot, self.getCycle, self.getStepA, self.getStepB, self.animLYRName)
+        return self.getRFootCtrl, self.getLFootCtrl, self.getFRangeStart, self.getFRangeEnd, self.getStartFoot, self.getCycle, self.getStepA, self.getStepB, self.animLYRName
+
+
+    def cycleClean(self, *args):
+        self.getData()              #-> self.getRFootCtrl, self.getLFootCtrl, self.getFRangeStart, self.getFRangeEnd, self.getStartFoot, self.getCycle, self.getStepA, self.getStepB, self.animLYRName
+        #Set which foot is in front.
+        if self.getStartFoot == 1:
+            footOrder = [self.getRFootCtrl, self.getLFootCtrl]
+        else:
+            footOrder = [self.getLFootCtrl, self.getRFootCtrl]
+        print(footOrder)
+        #Check if autokey is on and turn it off it is.
+        autoKey = mc.autoKeyframe(q=True, state=True)
+        if autoKey:
+            mc.autoKeyframe(state=False)
+        #If an animLayer is wanted, create and/or add the foot controls to it.
+        if self.animLYRName:
+            for foot in footOrder:
+                addToAnimLYR(foot, self.animLYRName)
+            focusAnimLyr(self.animLYRName)
+            print(self.animLYRName)
+        funcLoop(self.getFRangeStart, self.getFRangeEnd, self.getCycle, footOrder[0], self.getStepA, self.getStepB, self.animLYRName)
+        funcLoop(self.getFRangeStart, self.getFRangeEnd, self.getCycle, footOrder[1], self.getStepA+self.getCycle, self.getStepB+self.getCycle, self.animLYRName)
+
+        # if self.animLYRName:
+        #     halfStep = int(self.getCycle / 2)
+        #     for foot in footOrder:
+        #         for frame in range(self.getFRangeStart, self.getFRangeEnd, halfStep+self.getCycle):
+        #             animLyrZero(foot, frame,self.animLYRName)
+        
+        #If autokey was on when the tool was run, turn it back on.
+        if autoKey:
+            mc.autoKeyframe(state=True)
+
+        
         
 
 
